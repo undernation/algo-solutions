56f48de274df3f6b96975887758181263661e5b7
undernation <solomon2752@naver.com>
2026-09-17 03:41:59 +0000

    [SWEA 25958] 타워디펜스게임 — 틀림

--- swea/25958_타워디펜스게임.py
+++ swea/25958_타워디펜스게임.py
@@ -2,13 +2,15 @@
 SWEA 25958  타워디펜스게임
 https://swexpertacademy.com/main/talk/solvingClub/problemView.do?solveclubId=AZt8IiBqxEDHBIN6&contestProbId=AZvfDDtKDNjHBIN6&probBoxId=AZt8IiBqxEHHBIN6&type=PROBLEM
 
-풀이일 : 2026-09-17   결과: 품
+풀이일 : 2026-09-17   결과: 틀림
 한도   : time 25개 테스트케이스를 합쳐서 C++의 경우 3초 / Java의 경우 3초 / Python의 경우 5초 / memory 힙, 정적 메모리 합쳐서 262144 kbytes 이내, 스택 메모리 1024 kbytes 이내 / time_sec 5
 난이도 : D5  |  정답률 64.29%
 제약   : 1. 각 테스트 케이스 시작 시 init() 함수가 한 번 호출된다.
 제약   : 2. Map 의 크기는 최대 20 x 20 이다.
 제약   : 3. 각 테스트 케이스에서 addTower() 함수의 호출 횟수는 N x N 이하이다.
 제약   : 4. 각 테스트 케이스에서 runSimulation() 함수의 호출 횟수는 10 이하이다.
+
+[채점] accepted  1/1  (4.788s)
 
 [문제]
 타워디펜스게임을 시뮬레이션 하는 API 를 구현해보자.
