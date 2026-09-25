d719def189776488d3cd7a82f4fb346b185faf7a
undernation <solomon2752@naver.com>
2026-09-25 07:50:35 +0000

    [BOJ 14002] 가장 긴 증가하는 부분 수열 4 — 못품

--- boj/14002.py
+++ boj/14002.py
@@ -2,8 +2,10 @@
 BOJ 14002  가장 긴 증가하는 부분 수열 4
 https://cosal.aviss.kr/problems/detail/14002
 
-풀이일 : 2026-09-22   결과: 못품
+풀이일 : 2026-09-25   결과: 못품
 한도   : time 1 초 / memory 256 MB
+
+[채점] accepted  17/17  (4.672s)
 
 [문제]
 수열 A가 주어졌을 때, 가장 긴 증가하는 부분 수열을 구하는 프로그램을 작성하시오.
@@ -19,4 +21,29 @@ https://cosal.aviss.kr/problems/detail/14002
 10 20 30 50
 """
 
-.
+
+N = int(input())
+num_list = list(map(int, input().split()))
+parent = [-1] * N
+dp = [1] * N
+
+for i in range(N):
+
+    for j in range(i):
+        if num_list[j] < num_list[i]:
+            if dp[i] < dp[j] + 1:
+                dp[i] = max(dp[j] + 1, dp[i])
+                parent[i] = j
+
+# print(dp)
+# print(parent)
+
+max_idx = dp.index(max(dp))
+
+ret = []
+while max_idx != -1:
+    ret.append(num_list[max_idx])
+    max_idx = parent[max_idx]
+print(max(dp))
+ret.reverse()
+print(*ret)
