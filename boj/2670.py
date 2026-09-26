62017d23a6a88f5b51f05e38c3edace96f418c49
undernation <solomon2752@naver.com>
2026-09-26 04:51:52 +0000

    [BOJ 2670] 연속부분최대곱 — 품

--- boj/2670.py
+++ boj/2670.py
@@ -2,10 +2,10 @@
 BOJ 2670  연속부분최대곱
 https://cosal.aviss.kr/problems/detail/2670
 
-풀이일 : 2026-09-23   결과: 못품
+풀이일 : 2026-09-26   결과: 품
 한도   : time 1 초 / memory 128 MB
 
-[채점] accepted  27/27  (7.584s)
+[채점] accepted  27/27  (7.425s)
 
 [문제]
 N개의 실수가 있을 때, 한 개 이상의 연속된 수들의 곱이 최대가 되는 부분을 찾아, 그 곱을 출력하는 프로그램을 작성하시오. 예를 들어 아래와 같이 8개의 양의 실수가 주어진다면,
@@ -32,21 +32,16 @@ N개의 실수가 있을 때, 한 개 이상의 연속된 수들의 곱이 최�
 
 
 N = int(input())
-answer = 0
 num_list = []
 
-for n in range(N):
+for _ in range(N):
     num_list.append(float(input()))
 
 dp = [0.0] * N
 dp[0] = num_list[0]
-answer = dp[0]
-# dp[i] = max(arr[i], dp[i - 1] * arr[i])
+for idx in range(1, N):
+    dp[idx] = max(num_list[idx], dp[idx - 1] * num_list[idx])
 
-for i in range(1, N):
-    dp[i] = max(num_list[i], dp[i - 1] * num_list[i])
-
-    answer = max(answer, dp[i])
-
+answer = max(dp)
 
 print(f"{answer:.3f}")
